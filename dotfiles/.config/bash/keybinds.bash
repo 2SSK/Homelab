509401b8f715e6b44b11c787b46d5107371846e5
2SSK <100ravsinghkarmwar@gmail.com>
2025-12-31 15:46:06 +0530

    feat: improve bash configuration - add interactive shell check for keybindings - add battery icon to prompt

--- dotfiles/.config/bash/keybinds.bash
+++ dotfiles/.config/bash/keybinds.bash
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-# Ctrl+L to clear screen
-bind '"\C-l":clear-screen'
+# Only set keybindings in interactive shells
+if [[ $- == *i* ]]; then
+  # Ctrl+L to clear screen
+  bind '"\C-l":clear-screen'
 
-# Up/Down arrow for history search
-bind '"\e[A": history-search-backward'
-bind '"\e[B": history-search-forward'
-
-# Vi mode
-set -o vi
-bind '"jk": vi-movement-mode'
+  # Up/Down arrow for history search
+  bind '"\e[A": history-search-backward'
+  bind '"\e[B": history-search-forward'
+  
+  # Vi mode
+  set -o vi
+  bind '"jk": vi-movement-mode'
+fi
